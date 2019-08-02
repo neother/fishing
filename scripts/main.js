079f582db39b8a69a47cdd8d522939ed245adeba
neother <neother@126.com>
2019-08-02 22:51:14 +0800

    go

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,41 @@
-let randomNumber = Math.floor(Math.random() * 100) + 1;
 
-const resp = document.querySelector('.resp');
+$(document).ready(function () {
 
-x = 3
-const guessSubmit = document.querySelector('.guessSubmit');
 
-  function httprequest() {
-    xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", "http://hq.sinajs.cn/list=SR1909", false ); // false for synchronous request
-    xmlHttp.setRequestHeader("Access-Control-Allow-Methods", "GET");
-    xmlHttp.send( null );
-    return xmlHttp.responseText.toString();
-}
 
-function test() {
-        
-        resp.textContent = Math.floor(Math.random() * 1000) + 1;;
-        resp.style.fontSize = '200%';
-        console.log(resp.textContent);
-    }
 
-setInterval(test,1000
-    )
 
-  guessSubmit.addEventListener('click', test);
+  $("button").click(function () {
+    alert($("#1").html())
+    
+  });
+
+
+  // randomNumber = Math.floor(Math.random() * 100) + 1;
+
+
+  // // function httprequest() {
+  //   xmlHttp = new XMLHttpRequest();
+  //   xmlHttp.open("GET", "http://hq.sinajs.cn/list=SR1909", false); // false for synchronous request
+  //   xmlHttp.setRequestHeader("Access-Control-Allow-Methods", "GET");
+  //   xmlHttp.send(null);
+  //   return xmlHttp.responseText.toString();
+  // }
+
+
+
+
+  // function test() {
+
+  //   resp.textContent = Math.floor(Math.random() * 1000) + 1;;
+  //   resp.style.fontSize = '200%';
+
+  // }
+
+  // setInterval(test, 1000)
+
+  // guessSubmit.addEventListener('click', test);
+
+
+});
+
